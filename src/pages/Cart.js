47c49eaec006ee82db2457f27e9e9e13e87901b6
Manu Sucaria <manusucaria@gmail.com>
2022-11-20 16:41:27 -0300

    20 del 11 4

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,10 +5,23 @@ import '../styles/Cart.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+const MySwal = withReactContent(Swal);
 
 const Cart = () => {
     const {state, eliminarTodo} = useMyContext([]);
     const total = state.map(item => item.precio*item.cantidad).reduce((prev, curr) => prev + curr, 0);
+    function terminarCompra () {
+        MySwal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Compra Finalizada',
+            showConfirmButton: false,
+            timer: 1500
+        })
+    }
     return (
         <div className='body-carrito'>
             <h2 className='titulo-carrito'>Carrito</h2>
@@ -36,6 +49,7 @@ const Cart = () => {
                             <button onClick={() => eliminarTodo()} className='boton-eliminar'><p className='texto-eliminar'>Eliminar Todos los Productos</p></button>
                             <p className='total'>Valor Total: ${total}</p>
                         </div>
+                        <button onClick={terminarCompra} className='boton-eliminar'><p className='texto-eliminar'>Terminar La Compra</p></button>
                     </div>
                     : <Link to={process.env.PUBLIC_URL + "/productos"} className= "carritoVacio">Volver a Productos</Link>
                 }
